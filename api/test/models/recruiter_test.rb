2231187410ec9f3eb5eacf2b7050c260d28fa7fe
Onofre Souza <souzaonofre@gmail.com>
2024-05-25 13:57:21 -0300

    Finish model recruiter test with update & delete actions.

--- api/test/models/recruiter_test.rb
+++ api/test/models/recruiter_test.rb
@@ -30,4 +30,52 @@ class RecruiterTest < ActiveSupport::TestCase
 
   end
 
+  test "update recruiter" do
+    fake_data = {
+      :name =>  "Recruiter 001",
+      :email => "recruiter_001@gmail.com",
+      :password => "sdsadsdaddksldjakdajdaldaiuiwiwiwi",
+      :password_confirmation => "sdsadsdaddksldjakdajdaldaiuiwiwiwi"
+    }
+
+    recruiter = Recruiter.new fake_data
+    assert recruiter.save
+
+    update_fake_data = {
+      :name => "Recruiter 002",
+      :email => "recruiter_002@gmail.com"
+    }
+    assert recruiter.update update_fake_data
+    assert recruiter.name === update_fake_data[:name]
+    assert recruiter.email === update_fake_data[:email]
+
+  end
+
+  test "delete recruiter" do
+    fake_data = {
+      :name =>  "Recruiter 001",
+      :email => "recruiter_001@gmail.com",
+      :password => "sdsadsdaddksldjakdajdaldaiuiwiwiwi",
+      :password_confirmation => "sdsadsdaddksldjakdajdaldaiuiwiwiwi"
+    }
+
+    recruiter = Recruiter.new fake_data
+    assert recruiter.save
+    assert_respond_to recruiter, :id
+
+    found_recruiter = Recruiter.find recruiter.id
+    assert_not_nil found_recruiter
+
+    assert found_recruiter.delete
+    
+    after_delete_recruiter = nil
+    begin
+      after_delete_recruiter = Recruiter.find recruiter.id
+    rescue ActiveRecord::RecordNotFound
+    end
+    assert_nil after_delete_recruiter
+
+  end
+
+
 end
