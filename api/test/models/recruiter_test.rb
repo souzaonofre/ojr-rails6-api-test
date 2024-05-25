2304426c7afd9298580e55c640aa44e8cb92a5e1
Onofre Souza <souzaonofre@gmail.com>
2024-05-25 12:58:23 -0300

    Add model new recruiter test

--- api/test/models/recruiter_test.rb
+++ api/test/models/recruiter_test.rb
@@ -4,8 +4,35 @@ class RecruiterTest < ActiveSupport::TestCase
 
   test "instance" do
     recruiter = Recruiter.new
-    assert_not recruiter.nil? 
-    assert recruiter.instance_of? Recruiter
+    assert_not_nil recruiter 
+    assert_instance_of Recruiter, recruiter
+  end
+
+  test "new recruiter" do
+    fake_data = {
+      :name =>  "Recruiter 001",
+      :email => "recruiter_001@gmail.com",
+      :password => "sdsadsdaddksldjakdajdaldaiuiwiwiwi"
+    }
+
+    recruiter = Recruiter.new
+
+    recruiter.name = fake_data[:name]
+    recruiter.email = fake_data[:email]
+    recruiter.password = fake_data[:password]
+    recruiter.password_confirmation = fake_data[:password]
+    
+    assert recruiter.save
+
+    assert_respond_to recruiter, :name
+    assert recruiter.name === fake_data[:name]
+
+    assert_respond_to recruiter, :email
+    assert recruiter.email === fake_data[:email]
+
+    assert_respond_to recruiter, :password
+    assert recruiter.password === fake_data[:password]
+
   end
 
 end
