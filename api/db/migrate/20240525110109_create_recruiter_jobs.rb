4c9280bf9677e671ef598844e033ece379bfd21f
Onofre Souza <souzaonofre@gmail.com>
2024-05-27 16:59:13 +0000

    Change to appropriate index algorithm for "skills" array field.

--- api/db/migrate/20240525110109_create_recruiter_jobs.rb
+++ api/db/migrate/20240525110109_create_recruiter_jobs.rb
@@ -3,18 +3,20 @@ class CreateRecruiterJobs < ActiveRecord::Migration[6.1]
     create_table :recruiter_jobs do |t|
       t.string :title
       t.string :description
-      
+
       t.text :skills, array: true, default: []
 
       t.datetime :start_date
       t.datetime :end_date
+
       t.boolean :status
+      
       t.integer :recruiter_id
 
       t.timestamps
     end
 
-    add_index(:recruiter_jobs, :skills, using: 'btree')
+    add_index(:recruiter_jobs, :skills, using: 'gin')
     add_index(:recruiter_jobs, :status, using: 'btree')
 
   end
