e28e0bbae0e3097e4b97c80a8651ba06052e87dd
Onofre Souza <souzaonofre@gmail.com>
2024-05-30 14:17:21 -0300

    Add controller handling request excpetions related.

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
 class ApplicationController < ActionController::API
   include JsonJwtToken
+  rescue_from Exception, with: :handle_exception
 
   before_action :check_auth_jwt, except: :login
+
+  EXCEPTIONS_TO_CAPTURE = [
+    ActionController::RoutingError,
+    ActiveRecord::RecordNotFound,
+    ActionController::UnknownFormat
+  ]
+
+  def handle_exception(exception=nil)
+    if exception
+      if EXCEPTIONS_TO_CAPTURE.include?(exception.class)
+        render json: {:error => 'resource not found'}, status: 404 
+      else
+        render json: {:error => 'internal error'}, status: 500 
+      end
+    end
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 
   private
 
